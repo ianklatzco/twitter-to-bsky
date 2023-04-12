a579a349a7c970c5215ae5e22ea7d721c393fdc9
Ian Klatzco <iklatzco@gmail.com>
2023-04-12 20:32:15 +0200

    grab urls correctly

--- webapp/main.py
+++ webapp/main.py
@@ -75,30 +75,61 @@ def process_json(input_json=None):
     eee = json.loads(input_json)
     returnlist = []
     for person_youre_following in eee:
+        bsky_username = None
+        # if person_youre_following[0].get("username") != "ian5v":
+        #     continue
+        # import sys; sys.exit()
+    
+        # print(json.dumps(person_youre_following[0]))
 
         # 1. check the handle to see if they just registered twitterhandle.bsky.social
         # 2. 
 
-        # two items in a list, first is the bio and second is the pinned tweet
-        # TODO check bio
         bio_full_json = person_youre_following[0]
         pinned = person_youre_following[1]
+
         displayname = bio_full_json.get('name')
         username = bio_full_json.get('username')
         description = bio_full_json.get('description')
+        entities = bio_full_json.get("entities")
 
+        if not description: continue 
+        if not entities: continue
+        if not entities.get('description'): continue
 
-        if ( "bsky" in description ) :
-            # print(description.split())
+        if "🦋" not in description: continue
+
+        print(username)
+
+        # handles that are valid website (probably all of them) will get url shortened
+        # entities -> description -> urls
+        # entities -> url -> urls
+
+        desc_urls = entities.get('description').get('urls')
+        if not desc_urls: continue 
+
+    
+        # only one link in desc
+        if len(desc_urls) == 1:
+            bsky_username = entities.get('description').get('urls')[0].get('expanded_url')
+
+        # TODO
+        # 2+ links in desc
+        if len(desc_urls) >= 2:
+            # bsky_username = entities.get('description').get('urls')[0].get('expanded_url')
             pass
+            # bfly_index = description.index("🦋")
+            # bfly_index + 1 or + 2 where the url/handle starts
 
-        if ( "bsky" in displayname ) :
-            pass
-            # print(displayname)
-
-        bsky_username = get_bsky_username(description) # from bio
         if bsky_username == None: continue
 
+
+        bsky_username = bsky_username.replace("http://",'').replace("https://","")
+        print(bsky_username)
+
+        # bsky_username = get_bsky_username(description) # from bio
+
+        print('got here')
         session = atprototools.Session(os.environ.get("BSKY_USERNAME"), os.environ.get("BSKY_PASSWORD"))
         bsky_did = session.resolveHandle(bsky_username).json().get('did')
         profile_json = session.getProfile(bsky_did).json()
@@ -120,7 +151,7 @@ def process_json(input_json=None):
 async def handle(request):
     if request.method == 'POST':
         data = await request.post()
-        print(data)
+        # print(data)
         twitter_username = data.get("name")
         ccc = data.get('text')
         list_of_user_profiles_on_bsky = process_json(ccc)
@@ -164,7 +195,8 @@ async def handle(request):
                 """ + "\n".join(rows) + "</table>"
         ,content_type="text/html")
     else:
-        testdata = open("testdata.json", encoding='utf-8').read()
+        # testdata = open("testdata.json", encoding='utf-8').read()
+        testdata = open("phil-following.json", encoding='utf-8').read()
         return web.Response(text=f"""
             <html>
                 <body>
@@ -196,6 +228,5 @@ def main():
                     web.post('/', handle)])
     web.run_app(app)
 
-# process_json()
 # test_get_bsky_username()
 main()
